ce3573893e1aa61bfe5667c3747c7fefe1dfe14d
thewinger <winger.87@gmail.com>
2023-03-24 10:53:52 +0100

    removes filterbar for now

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
         </div>
       </div>
       <div className="flex items-center gap-2 border-b-2 border-zinc-200 px-4 py-2 md:px-6">
-        <FilterBar />
+        {/* <FilterBar /> */}
       </div>
     </header>
   )
