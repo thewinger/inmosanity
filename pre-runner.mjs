6604f10f20b656f9e74f1f9144d22921163660e1
thewinger <winger.87@gmail.com>
2023-05-26 08:41:41 +0200

    update prerunner

---
+++ pre-runner.mjs
@@ -0,0 +1,27 @@
+console.log('********** PREBUILDING **********')
+
+import fs from 'fs'
+import path from 'path'
+/**
+ * EXPERIMENTAL: Hacking nextjs internals
+ */
+const baseDir = process.cwd()
+
+const prebuildScripts = async () => {
+  const file = path.join(
+    baseDir,
+    '/node_modules',
+    'next/dist/server/require-hook.js'
+  )
+
+  const content = await fs.promises.readFile(file, 'utf-8')
+  await fs.promises.writeFile(
+    file,
+    content.replace(
+      'if (process.env.__NEXT_PRIVATE_PREBUNDLED_REACT) {',
+      'if (true) {'
+    )
+  )
+}
+
+prebuildScripts()
