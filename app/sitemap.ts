96e04599bac3739cdac502a655009161b245d2dc
thewinger <winger.87@gmail.com>
2023-05-30 09:04:51 +0200

    updates sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,32 @@
+import { i18n } from '@/i18n-config'
+import { getAllPropiedadesSlug } from '@/lib/sanity.client'
 import { MetadataRoute } from 'next'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://inmogolfbonalba.com',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://inmogolfbonalba.com/aviso-legal',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://inmogolfbonalba.com/blog',
-      lastModified: new Date(),
-    },
-  ]
+const URL = 'https://inmogolfbonalba.com'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const slugs = await getAllPropiedadesSlug()
+  const locales = i18n.locales
+
+  const propiedades = locales!.flatMap((locale) => {
+    return slugs!.map((slug) => {
+      return {
+        url: `${URL}/${locale}/propiedad/${slug}`,
+        lastModified: new Date().toISOString(),
+      }
+    })
+  })
+
+  const routes = ['', '/aviso-legal']
+
+  const staticRoutes = locales!.flatMap((locale) => {
+    return routes!.map((route) => {
+      return {
+        url: `${URL}/${locale}/${route}`,
+        lastModified: new Date().toISOString(),
+      }
+    })
+  })
+
+  return [...staticRoutes, ...propiedades]
 }
