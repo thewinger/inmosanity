1be96a49e2ee71807eb55ea6108a70d901b824d2
Alejandro Camara <alejandro.camara@unisys.altia.es>
2026-02-04 15:39:49 +0100

    fix: restore 'use client' and hardcode studio metadata

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,11 +1,7 @@
+'use client'
+
 import { NextStudio } from 'next-sanity/studio'
 import config from 'sanity.config'
-
-// Pre-render at build time to avoid serverless timeout
-export const dynamic = 'force-static'
-
-// Re-export metadata and viewport from next-sanity/studio
-export { metadata, viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
