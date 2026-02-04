1dec3660bc469acedd1942598891d5c9d5492a48
Alejandro Camara <alejandro.camara@unisys.altia.es>
2026-02-04 16:18:24 +0100

    fix: add generateStaticParams to pre-render /studio path

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -4,6 +4,11 @@ import config from '../../../sanity.config'
 export const dynamic = 'force-static'
 export { metadata, viewport } from 'next-sanity/studio'
 
+// Pre-generate the root studio path, catch-all handles client-side navigation
+export function generateStaticParams() {
+  return [{ index: [] }]
+}
+
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
